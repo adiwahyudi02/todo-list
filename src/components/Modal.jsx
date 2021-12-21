38d042cf112111caf2df35f6370ec3edf119479c
adiwahyudi02 <adi.whyd02@gmail.com>
2021-12-21 19:49:01 +0700

    Fixing bug styling padding on modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ title, show, setShow, children }) => {
         show && (
             <>
                 <div className="max-w-xl min-h-fit m-auto mt-20 rounded-2xl bg-white justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none">
-                    <div className="w-full w-auto my-6 mx-auto">
+                    <div className="w-full my-6 mx-auto">
                         <div className="border-0 rounded-lg  flex flex-col w-full outline-none">
                             <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                                 <h3 className="text-3xl font-semibold">
